585215963b6f98f1399785d12b4f91097a4b2822
mod kaf <mhkaif7411@gmail.com>
2023-09-29 10:10:04 +0530

    add enviornment variable

---
+++ app.js
@@ -0,0 +1,89 @@
+//jshint esversion:6
+
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption")
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:['password']});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+        .then(() => {
+            res.render("secrets");
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username })
+        .then(foundUser => {
+            if (foundUser && foundUser.password === password) {
+                res.render("secrets");
+            } else {
+                // Handle case where user is not found or password is incorrect
+                res.render("login"); // You can render a login error message here
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            // Handle other errors, e.g., database connection issues
+        });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
