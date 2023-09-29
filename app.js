b3ec5d8c623ae0284c4bcca63dcea1ae376c0962
mod kaf <mhkaif7411@gmail.com>
2023-09-29 13:07:03 +0530

    hash value changes succesfully

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:['password']});
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,7 +39,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save()
@@ -55,7 +55,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username })
         .then(foundUser => {
@@ -85,5 +85,5 @@ app.post("/login", (req, res) => {
 
 
 app.listen(3000, () => {
-    console.log("Server started onfdknlk port 3000");
+    console.log("Server started on port 3000 and health is good");
 });
